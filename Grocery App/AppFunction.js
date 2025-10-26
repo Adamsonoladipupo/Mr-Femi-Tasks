2b62485cd66ecbe3f50aa38cd0f67a1792af704e
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-26 11:10:06 +0100

    Grocery app

---
+++ Grocery App/AppFunction.js
@@ -0,0 +1,6 @@
+function AddItem(item, cart){
+	cart = cart + item;
+	let message = "Item added successfully";
+	return message;
+	
+}
--- Grocery App/GroceryManagerApp.js
+++ Grocery App/GroceryManagerApp.js
@@ -1,23 +1,40 @@
 const prompt = require("prompt-sync")({sigint: true});
 
-let cart = [];
-console.log("Welcome to The Grocery Store \n1. Add item \n2. Remove item \n3. Show all available items");
-let userInput = prompt("Make a selection: ");
-switch (userInput){
-	case "1": {
-		let userInput = prompt("Enter item: ");
-		cart = cart + userInput;
-		console.log("%s added successfully", userInput);
-
-	}
-	case "2": {console.log("remove item")}
-	case "3": {
-		console.log("Available Items")
-		for (let count = 0; count<= cart.length; count++){
-			console.log("%d. %s", count+1, cart[count])
-		}
-
-	}
-	default: {console.log("Invalid input, please select from the options")}
-
+// Functions
+function AddItem(item, cart){
+	cart.push(item);
+	let message = "Item added successfully";
+	return message;	
 }
+function RemoveItem(item, cart){
+	cart.pop(item);
+	let message = "Item remove successfully";
+	return message;	
+}
+
+
+let cart = [];
+
+while (true){
+	console.log("----------------------------------- \nWelcome to The Grocery Store \n1. Add item \n2. Remove item \n3. Show all available items \n----------------------------------");
+	let userInput = prompt("Make a selection: ");
+	if (userInput == 1){
+		let userInput = prompt("What item are you adding: ");
+		let AddingItem = AddItem(userInput, cart);
+		console.log("'%s' added successfully",userInput);
+	}
+	if (userInput == 2){
+		let userInput = prompt("What item are you removing: ");
+		let RemovingItem = RemoveItem(userInput, cart);
+		console.log("'%s' removed successfully",userInput);
+	}
+	if (userInput == 3){
+		console.log("Available items in the cart:")
+		for (let count = 0; count < cart.length; count++){
+			console.log("%d. %s", count+1, cart[count]);
+		}
+	}
+	if (userInput != 1 && userInput != 2 && userInput != 3){
+		console.log("Invalid input, please select from the options")
+	}
+} // end of while loop
